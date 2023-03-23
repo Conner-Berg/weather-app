f2bd6bc7540b344c1ef088973e1a63ad53a125ba
Conner-Berg <connerwberg@gmail.com>
2023-03-23 15:45:07 -0400

    Add user input logic to text box and submit btn

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-function logWeatherAPI() {
+const locationInput = document.querySelector(".location-input");
+const submit = document.querySelector(".location-submit");
+
+function logWeatherAPI(event) {
+	event.preventDefault();
 	fetch(
-		"http://api.openweathermap.org/data/2.5/weather?q=London&APPID=7208fe965623c8f42299986e1f01866b",
+		`http://api.openweathermap.org/data/2.5/weather?q=${locationInput.value}&APPID=7208fe965623c8f42299986e1f01866b`,
 		{ mode: "cors" }
 	)
 		.then(function (response) {
@@ -11,4 +15,4 @@ function logWeatherAPI() {
 		});
 }
 
-logWeatherAPI();
+submit.addEventListener("click", logWeatherAPI);
