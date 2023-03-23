2d8b8953d1add3cc7dbfdb777adc96a3942d0af3
Conner-Berg <connerwberg@gmail.com>
2023-03-23 15:33:27 -0400

    Add base dev testing env and form skeleton

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function logWeatherAPI() {
 	fetch(
-		"http://api.openweathermap.org/data/2.5/weather?q=Tampa&APPID=7208fe965623c8f42299986e1f01866b",
+		"http://api.openweathermap.org/data/2.5/weather?q=London&APPID=7208fe965623c8f42299986e1f01866b",
 		{ mode: "cors" }
 	)
 		.then(function (response) {
